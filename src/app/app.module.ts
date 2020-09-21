c443bc5533792e7557ed01ff61f74dfb8f5f542b
deejayy <deejayy@deejayy.hu>
2020-09-21 20:25:40 +0200

    Add api caller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,12 @@ import { BemModule } from 'angular-bem';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { ApiCallerModule } from '@deejayy/api-caller';
+import { environment } from '@env/environment';
+
 BemModule.config({
   separators: ['-', '_', ''],
   modCase: 'kebab',
@@ -17,7 +23,20 @@ BemModule.config({
 
 @NgModule({
   declarations: [AppComponent, AppFrameComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, BemModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    BemModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot(),
+    StoreDevtoolsModule.instrument({
+      name: 'One Identity THC',
+      maxAge: 200,
+      logOnly: environment.production,
+    }),
+    ApiCallerModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
