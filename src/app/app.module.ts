f7302e74e2e85f12da6a9dcbe39086360392ad86
deejayy <deejayy@deejayy.hu>
2020-09-21 20:10:34 +0200

    Create basic sitebuild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppFrameComponent } from './shared/app-frame/app-frame.component';
 
+import { BemModule } from 'angular-bem';
+
+BemModule.config({
+  separators: ['-', '_', ''],
+  modCase: 'kebab',
+  ignoreValues: false,
+});
+
 @NgModule({
   declarations: [AppComponent, AppFrameComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BemModule],
   providers: [],
   bootstrap: [AppComponent],
 })
