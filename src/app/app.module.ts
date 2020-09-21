68c5817c0d4b0438ce44ac366d92c7272b12ee67
deejayy <deejayy@deejayy.hu>
2020-09-21 20:15:01 +0200

    Add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppFrameComponent } from './shared/app-frame/app-frame.component';
 
 import { BemModule } from 'angular-bem';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 BemModule.config({
   separators: ['-', '_', ''],
   modCase: 'kebab',
@@ -15,7 +17,7 @@ BemModule.config({
 
 @NgModule({
   declarations: [AppComponent, AppFrameComponent],
-  imports: [BrowserModule, AppRoutingModule, BemModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, BemModule],
   providers: [],
   bootstrap: [AppComponent],
 })
