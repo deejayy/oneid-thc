5358aefe2a6dde663432a5f9a4fdaa740a2ef6f8
deejayy <deejayy@deejayy.hu>
2020-09-23 00:34:02 +0200

    Testing, 1, 2, 3...

--- src/app/feature/modify-user/modify-user.component.spec.ts
+++ src/app/feature/modify-user/modify-user.component.spec.ts
@@ -1,6 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ModifyUserComponent } from './modify-user.component';
+import { CommonModule } from '@angular/common';
+import { MatDialogModule, MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { BemModule } from 'angular-bem';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { ApiCallerService } from '@deejayy/api-caller';
+import { of } from 'rxjs';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+const mockApiCallerService = {
+  createApiResults: jest.fn().mockReturnValue({ data$: of(), loading$: of(false) }),
+  callApi: jest.fn(),
+}
+const mockMatDialog = {}
+const mockMatDialogRef = {}
 
 describe('ModifyUserComponent', () => {
   let component: ModifyUserComponent;
@@ -8,9 +24,15 @@ describe('ModifyUserComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ModifyUserComponent ]
-    })
-    .compileComponents();
+      imports: [CommonModule, MatDialogModule, MatButtonModule, BemModule, ReactiveFormsModule, MatInputModule, NoopAnimationsModule],
+      declarations: [ModifyUserComponent],
+      providers: [
+        { provide: ApiCallerService, useValue: mockApiCallerService },
+        { provide: MatDialog, useValue: mockMatDialog },
+        { provide: MatDialogRef, useValue: mockMatDialogRef },
+        { provide: MAT_DIALOG_DATA, useValue: { data: { id: 1 } } },
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
