95b1cb226785f90da601052b8889467a92982a70
deejayy <deejayy@deejayy.hu>
2020-09-23 12:14:13 +0200

    Make tests pass 100%

--- src/app/feature/delete-user/delete-user.component.spec.ts
+++ src/app/feature/delete-user/delete-user.component.spec.ts
@@ -1,6 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DeleteUserComponent } from './delete-user.component';
+import { MatDialogRef, MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ApiCallerService } from '@deejayy/api-caller';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { BemModule } from 'angular-bem';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { of } from 'rxjs';
+
+const mockApiCallerService = {
+  createApiResults: jest.fn().mockReturnValue({ data$: of(), loading$: of(false) }),
+  callApi: jest.fn(),
+}
+const mockMatDialog = {}
+const mockMatDialogRef = {}
 
 describe('DeleteUserComponent', () => {
   let component: DeleteUserComponent;
@@ -8,9 +22,14 @@ describe('DeleteUserComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DeleteUserComponent ]
-    })
-    .compileComponents();
+      imports: [CommonModule, MatDialogModule, MatButtonModule, BemModule, NoopAnimationsModule],
+      declarations: [DeleteUserComponent],
+      providers: [
+        { provide: ApiCallerService, useValue: mockApiCallerService },
+        { provide: MatDialog, useValue: mockMatDialog },
+        { provide: MatDialogRef, useValue: mockMatDialogRef },
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
